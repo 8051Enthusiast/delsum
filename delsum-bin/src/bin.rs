8ebe3bc3b0bff93152ed71f295faec966e1d8384
8051Enthusiast <8051enthusiast@protonmail.com>
2020-11-11 06:41:54 +0100

    change to structop

--- delsum-bin/src/bin.rs
+++ delsum-bin/src/bin.rs
@@ -1,148 +1,71 @@
-use clap::{App, Arg, ArgGroup};
 use delsum::checksum::{RelativeIndex, Relativity};
 use delsum::{find_algorithm, find_checksum_segments};
+use structopt::StructOpt;
 #[cfg(feature = "parallel")]
 use rayon::prelude::*;
 use std::fs::File;
 use std::io::Read;
 use std::process::exit;
+use std::ffi::OsString;
 
 fn main() {
-    #[cfg(feature = "parallel")]
-    let parallel_string = "try doing more parallelism, in turn using more memory";
-    #[cfg(not(feature = "parallel"))]
-    let parallel_string = "try doing more parallelism, in turn using more memory (not available because disabled at compile time)";
-    let matches = App::new("delsum")
-        .version("0.1.0")
-        .author("8051Enthusiast <8051enthusiast@protonmail.com>")
-        .about("Finds segments with given checksums inside files")
-        .arg(
-            Arg::with_name("verbose")
-                .short("v")
-                .long("verbose")
-                .multiple(true)
-                .help("put some info on what the program is currently doing to stderr")
-        )
-        .arg(
-            Arg::with_name("model")
-                .short("m")
-                .long("model")
-                .value_name("MODEL STRING")
-                .help("use the checksum algorithm given by the model string")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("modelfile")
-                .short("M")
-                .long("modelfile")
-                .value_name("FILE")
-                .help("read model strings line-by-line from given file"),
-        )
-        .group(
-            ArgGroup::with_name("models")
-                .arg("model")
-                .arg("modelfile")
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("start")
-                .help("sets the end of the checksum segments to be relative to the start of the file (default)")
-                .long("start")
-                .short("s")
-        )
-        .arg(
-            Arg::with_name("end")
-                .help("sets the end of the checksum segments to be relative to the end of the file")
-                .long("end")
-                .short("e")
-        )
-        .group(
-            ArgGroup::with_name("relativity")
-                .arg("start")
-                .arg("end")
-        )
-        .arg(
-            Arg::with_name("parallel")
-                .help(
-                    parallel_string
-                )
-                .long("parallel")
-                .short("p")
-        )
-        .arg(
-            Arg::with_name("checksums")
-                .help("a comma-separated list of checksums to match")
-                .short("c")
-                .long("checksum")
-                .value_name("CHECKSUMS")
-                .required(true)
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("reverse")
-                .help("reverse")
-                .short("r")
-                .long("reverse")
-        )
-        .arg(
-            Arg::with_name("files")
-                .help("the files to find checksummed segments of")
-                .index(1)
-                .min_values(1),
-        )
-        .get_matches();
-    let verbosity = matches.occurrences_of("verbose");
-    let files = matches.values_of_os("files").unwrap();
-    let mut bytes = Vec::new();
-    for file in files {
-        let mut current_bytes = Vec::new();
-        File::open(file)
-            .unwrap_or_else(|err| {
-                eprintln!("Could not open file '{}': {}", file.to_string_lossy(), err);
-                exit(1);
-            })
-            .read_to_end(&mut current_bytes)
-            .unwrap_or_else(|err| {
-                eprintln!("Could not read file '{}': {}", file.to_string_lossy(), err);
-                exit(1);
-            });
-        bytes.push(current_bytes);
+    let opt = Opt::from_args();
+    match opt {
+        Opt::Part(p) => part(&p),
+        Opt::Reverse(r) => reverse(&r),
     }
-    let models = matches.value_of_os("modelfile").map_or_else(
-        || vec![matches.value_of("model").map(String::from).unwrap()],
-        |file| {
-            let mut s = String::new();
-            File::open(file)
-                .unwrap_or_else(|err| {
-                    eprintln!("Could not open file '{}': {}", file.to_string_lossy(), err);
-                    exit(1);
-                })
-                .read_to_string(&mut s)
-                .unwrap_or_else(|err| {
-                    eprintln!("Could not read file '{}': {}", file.to_string_lossy(), err);
-                    exit(1);
-                });
-            s.lines()
-                .filter(|x| !x.is_empty() && !x.starts_with('#'))
-                .map(String::from)
-                .collect()
-        },
-    );
-    let checksums = matches.value_of("checksums").unwrap();
-    let rel = if matches.is_present("end") {
-        Relativity::End
-    } else {
-        Relativity::Start
-    };
+}
 
+fn reverse(opts: &Reverse) {
+    let files = read_files(&opts.files);
+    let models = read_models(&opts.model, &opts.model_file);
+    let byte_slices: Vec<_> = files.iter().map(Vec::<u8>::as_slice).collect();
+    let algorithms = |model: &str| {
+        find_algorithm(&model, &byte_slices, &opts.checksums, opts.verbose).unwrap_or_else(|err| {
+            eprintln!("Could not process model '{}': {}", model, err);
+            exit(1);
+        })
+    };
     #[cfg(feature = "parallel")]
-    let parallel = matches.is_present("parallel");
+    let parallel = opts.parallel;
     #[cfg(not(feature = "parallel"))]
     let parallel = false;
-    let reverse = matches.is_present("reverse");
-    let byte_slices: Vec<_> = bytes.iter().map(Vec::<u8>::as_slice).collect();
+
+    match parallel {
+        true => {
+            #[cfg(feature = "parallel")]
+            models.par_iter().for_each(|x| {
+                algorithms(x).find_all_para().for_each(|algo| match algo {
+                    Ok(a) => println!("{}", a),
+                    Err(e) => eprintln!("Error on {}: {}", x, e),
+                })
+            });
+        }
+        false => {
+            models.iter().for_each(|x| {
+                algorithms(x).find_all().for_each(|algo| match algo {
+                    Ok(a) => println!("{}", a),
+                    Err(e) => eprintln!("Error on {}: {}", x, e),
+                })
+            });
+        }
+    }
+}
+
+fn part(opts: &Part) {
+    let files = read_files(&opts.files);
+    let models = read_models(&opts.model, &opts.model_file);
+    let rel = if opts.start {
+        Relativity::Start
+    } else {
+        Relativity::End
+    };
+    #[cfg(feature = "parallel")]
+    let parallel = opts.parallel;
+    #[cfg(not(feature = "parallel"))]
+    let parallel = false;
     let subsum_print = |model| {
-        let segs = find_checksum_segments(model, &bytes, checksums, rel).unwrap_or_else(|err| {
+        let segs = find_checksum_segments(model, &files, &opts.checksums, rel).unwrap_or_else(|err| {
             eprintln!("Could not process model '{}': {}", model, err);
             exit(1);
         });
@@ -166,36 +89,119 @@ fn main() {
             }
         }
     };
-    let algorithms = |model: &str| {
-        find_algorithm(model, &byte_slices, checksums, verbosity).unwrap_or_else(|err| {
-            eprintln!("Could not process model '{}': {}", model, err);
-            exit(1);
-        })
-    };
-    match (reverse, parallel) {
-        (true, true) => {
-            #[cfg(feature = "parallel")]
-            models.par_iter().for_each(|x| {
-                algorithms(x).find_all_para().for_each(|algo| match algo {
-                    Ok(a) => println!("{}", a),
-                    Err(e) => eprintln!("Error on {}: {}", x, e),
-                })
-            });
-        }
-        (true, false) => {
-            models.iter().for_each(|x| {
-                algorithms(x).find_all().for_each(|algo| match algo {
-                    Ok(a) => println!("{}", a),
-                    Err(e) => eprintln!("Error on {}: {}", x, e),
-                })
-            });
-        }
-        (false, true) => {
+    match parallel {
+        true => {
             #[cfg(feature = "parallel")]
             models.par_iter().map(|x| x.as_str()).for_each(subsum_print);
         }
-        (false, false) => {
+        false => {
             models.iter().map(|x| x.as_str()).for_each(subsum_print);
         }
+    };
+}
+
+#[derive(Debug, StructOpt)]
+enum Opt {
+    Part(Part),
+    Reverse(Reverse),
+}
+
+/// With given checksum algorithm and checksums, find parts of the file matching the checksum
+#[derive(Debug, StructOpt)]
+#[structopt(rename_all = "kebab-case")]
+struct Part {
+    /// Print some messages indicating progress
+    #[structopt(short, long, parse(from_occurrences))]
+    verbose: u64,
+    /// Sets the end of the checksum segments to be relative to the start of the file
+    #[structopt(short, long)]
+    start: bool,
+    /// Sets the end of the checksum segments to be relative to the end of the file (default)
+    #[structopt(short, long)]
+    end: bool,
+    /// Do more parallelism, in turn using more memory
+    #[structopt(short, long)]
+    parallel: bool,
+    /// Use the checksum algorithm given by the model string
+    #[structopt(short, long)]
+    model: Option<String>,
+    /// Read model strings line-by-line from given file
+    #[structopt(short = "M", long)]
+    model_file: Option<OsString>,
+    /// A comma separated list of checksums, each corresponding to a file
+    #[structopt(short, long)]
+    checksums: String,
+    /// The files of which to find checksummed parts
+    files: Vec<OsString>
+}
+
+/// From given files and checksums, find out the checksum algorithms
+#[derive(Debug, StructOpt)]
+#[structopt(rename_all = "kebab-case")]
+struct Reverse {
+    /// Print some messages indicating progress
+    #[structopt(short, long, parse(from_occurrences))]
+    verbose: u64,
+    /// Do more parallelism, in turn using more memory
+    #[structopt(short, long)]
+    parallel: bool,
+    /// Use the checksum algorithm given by the model string
+    #[structopt(short, long)]
+    model: Option<String>,
+    /// Read model strings line-by-line from given file
+    #[structopt(short = "M", long)]
+    model_file: Option<OsString>,
+    /// A comma separated list of checksums, each corresponding to a file
+    #[structopt(short, long)]
+    checksums: String,
+    /// The files of which to find checksummed parts
+    files: Vec<OsString>
+}
+
+fn read_models(model: &Option<String>, model_file: &Option<OsString>) -> Vec<String> {
+    model_file.clone().map_or_else(
+        || {
+            let model = model.clone().unwrap_or_else(|| {
+                eprintln!("Need at least one of -m/-M",);
+                exit(1);
+            });
+            vec![model]
+        },
+        |file| {
+            let mut s = String::new();
+            File::open(&file)
+                .unwrap_or_else(|err| {
+                    eprintln!("Could not open file '{}': {}", &file.to_string_lossy(), err);
+                    exit(1);
+                })
+                .read_to_string(&mut s)
+                .unwrap_or_else(|err| {
+                    eprintln!("Could not read file '{}': {}", &file.to_string_lossy(), err);
+                    exit(1);
+                });
+            s.lines()
+                .filter(|x| !x.is_empty() && !x.starts_with('#'))
+                .map(String::from)
+                .collect()
+        },
+    )
+}
+
+fn read_files(files: &[OsString]) -> Vec<Vec<u8>> {
+    let mut bytes = Vec::new();
+    for file in files {
+        let mut current_bytes = Vec::new();
+        File::open(file)
+            .unwrap_or_else(|err| {
+                eprintln!("Could not open file '{}': {}", file.to_string_lossy(), err);
+                exit(1);
+            })
+            .read_to_end(&mut current_bytes)
+            .unwrap_or_else(|err| {
+                eprintln!("Could not read file '{}': {}", file.to_string_lossy(), err);
+                exit(1);
+            });
+        bytes.push(current_bytes);
     }
+    bytes
 }
