278c2799d53cf5a3329ef639d352d6f98c245390
8051Enthusiast <8051enthusiast@protonmail.com>
2024-01-28 00:03:08 +0100

    remove gmp from ntl
    hopefully makes the binary smaller and also makes the
    ci not fail because of gmp's github ban

--- build.sh
+++ build.sh
@@ -6,8 +6,6 @@ set -eu -o pipefail
 
 GF2X_VERSION=1.3.0
 GF2X_LINK="https://gitlab.inria.fr/gf2x/gf2x/uploads/c46b1047ba841c20d1225ae73ad6e4cd/gf2x-$GF2X_VERSION.tar.gz"
-GMP_VERSION=6.3.0
-GMP_LINK="https://gmplib.org/download/gmp/gmp-$GMP_VERSION.tar.lz"
 NTL_VERSION=11.5.1
 NTL_LINK="https://shoup.net/ntl/ntl-$NTL_VERSION.tar.gz"
 
@@ -24,17 +22,12 @@ cd gf2x-$GF2X_VERSION
 make -j "$(nproc)"
 make install
 cd ..
-wget "$GMP_LINK"
-tar xf gmp-$GMP_VERSION.tar.lz
-cd gmp-$GMP_VERSION
-./configure --disable-shared --enable-static --prefix="$PREFIX"
-make -j "$(nproc)"
-make install
-cd ..
 wget "$NTL_LINK"
 tar xf ntl-$NTL_VERSION.tar.gz
 cd ntl-$NTL_VERSION/src
-./configure NTL_GF2X_LIB=on DEF_PREFIX="$PREFIX" NATIVE=off
+# note: we do not really use integer functionality from ntl, so we
+# don't need gmp either
+./configure NTL_GF2X_LIB=on NTL_GMP_LIP=off DEF_PREFIX="$PREFIX" NATIVE=off
 make -j "$(nproc)"
 make install
 popd
