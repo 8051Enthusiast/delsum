f4551b2e399faacda44533c75b04a9d540084efb
8051enthusiast <8051enthusiast@protonmail.com>
2025-05-10 20:58:24 +0200

    add `part` to delsum-web

--- delsum-web/src/lib.rs
+++ delsum-web/src/lib.rs
@@ -22,6 +22,35 @@ impl Guest for Delsum {
             .map(|x| x.map_err(|e| e.to_string()))
             .collect()
     }
+
+    fn part(files: Vec<ChecksummedFile>, model: String) -> Result<Vec<ChecksumRanges>, String> {
+        if files.is_empty() {
+            return Ok(vec![]);
+        }
+        let sums = files.iter().map(|x| x.checksum.clone()).collect::<Vec<_>>();
+        let bytes = files.into_iter().map(|x| x.file).collect::<Vec<_>>();
+        let min_len = bytes.iter().map(|x| x.len()).min().unwrap() as isize;
+        if min_len == 0 {
+            return Ok(vec![]);
+        }
+
+        match delsum_lib::find_checksum_segments(
+            &model,
+            &bytes,
+            SegmentChecksum::Constant(&sums),
+            SignedInclRange::new(0isize, min_len - 1).unwrap(),
+            SignedInclRange::new(0isize, min_len - 1).unwrap(),
+        ) {
+            Ok(res) => Ok(res
+                .into_iter()
+                .map(|(start, end)| ChecksumRanges {
+                    start: start.into_iter().map(|x| x as i32).collect(),
+                    end: end.into_iter().map(|x| x as i32).collect(),
+                })
+                .collect()),
+            Err(err) => Err(err.to_string()),
+        }
+    }
 }
 
 export!(Delsum);
